54e5ed5d715bd03827aee8cf0451e6c96b088141
“Brandon <balvarez325@gmail.com>
2022-11-16 16:12:48 -0500

    set routes and components

--- my-portfolio/src/components/Main/Main.jsx
+++ my-portfolio/src/components/Main/Main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import Home from "../Home/Home.jsx";
+import About from "../About/About.jsx";
+import Projects from "../Projects/Projects.jsx";
+import Resume from "../Resume/Resume.jsx";
+import {Routes, Route} from "react-router-dom";
+import "./Main.css";
 
 function Main() {
   return (
-    <div className='main'>Main</div>
-  )
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<Resume />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default Main
+export default Main;

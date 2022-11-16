fd524f89da2c1b4f5aaefe71ad06fca5c8d74bb9
“Brandon <balvarez325@gmail.com>
2022-11-16 16:24:45 -0500

    update Home.css

--- my-portfolio/src/components/Home/Home.jsx
+++ my-portfolio/src/components/Home/Home.jsx
@@ -3,7 +3,9 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <h2 className='home-title'>Home</h2>
+    </div>
   )
 }
 

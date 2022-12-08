4f14d19e5e3e4fdd908dfecbaafc4f9b3aed14fb
Brandon-Alvarez-03 <100237632+Brandon-Alvarez-03@users.noreply.github.com>
2022-12-07 19:14:30 -0500

    techstack complete

--- my-portfolio/src/components/About/Techstack.js
+++ my-portfolio/src/components/About/Techstack.js
@@ -24,18 +24,26 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <p style={{fontSize: ".18em"}}>React.js</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <p style={{fontSize: ".18em"}}>Python</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiDjango />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+        <p style={{fontSize: ".18em"}}>MongoDB</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+        <p style={{fontSize: ".18em"}}>PostgreSQL</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <p style={{fontSize: ".18em"}}>Git</p>
       </Col>
     </Row>
   );

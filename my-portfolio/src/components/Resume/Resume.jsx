55165c1edcf8aabe361efb4f508a88118fe0ddf4
Brandon-Alvarez-03 <100237632+Brandon-Alvarez-03@users.noreply.github.com>
2022-12-08 11:20:44 -0500

    resume annotation removed

--- my-portfolio/src/components/Resume/Resume.jsx
+++ my-portfolio/src/components/Resume/Resume.jsx
@@ -6,6 +6,7 @@ import pdf from "../../assets/Brandon_Alvarez_Resume.pdf";
 import {AiOutlineDownload} from "react-icons/ai";
 import {Document, Page, pdfjs} from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
@@ -21,7 +22,6 @@ function Resume() {
   return (
     <div>
       <Container fluid className="resume-section">
-        <Particle />
 
         <Row style={{justifyContent: "center", position: "relative"}}>
           <Button
@@ -37,7 +37,7 @@ function Resume() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 786 ? 1.4 : 0.6} />
           </Document>
         </Row>
 

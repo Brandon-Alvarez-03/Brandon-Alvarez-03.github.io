ba3fb1b151f5e8fbb4a71d1d7d5131c7b2ea7fc5
Brandon Alvarez <balvarez325@gmail.com>
2023-01-08 19:54:12 -0500

    update resume projects

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {Container, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../assets/Brandon_Alvarez_Res.pdf";
+import pdf from "../../assets/Brandon_Alvarez_Resume.pdf";
 import {AiOutlineDownload} from "react-icons/ai";
 import {Document, Page, pdfjs} from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";

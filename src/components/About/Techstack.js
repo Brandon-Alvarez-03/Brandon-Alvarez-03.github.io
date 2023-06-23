a27fd5fbfbbbf7d682057b8d6c3df27617dd2282
Brandon Alvarez <balvarez325@gmail.com>
2023-06-23 00:43:52 -0400

    revamp about page

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -12,8 +12,21 @@ import {
   DiScrum,
   DiCss3,
   DiHtml5,
+  DiRust,
 } from "react-icons/di";
-import {SiExpress, SiTypescript, SiFlask, SiAutodesk} from "react-icons/si";
+import {
+  SiExpress,
+  SiTypescript,
+  SiFlask,
+  SiAutodesk,
+  SiTailwindcss,
+  SiNextdotjs,
+  SiAmazonaws,
+  SiDocker,
+  SiJenkins,
+  SiGraphql,
+  SiSolidity,
+} from "react-icons/si";
 import {BsBootstrapFill} from "react-icons/bs";
 
 function Techstack() {
@@ -37,12 +50,20 @@ function Techstack() {
           <p style={{fontSize: ".18em"}}>Django</p>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
+          <DiRust />
+          <p style={{fontSize: ".18em"}}>Rust</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
           <DiNodejs />
           <p style={{fontSize: ".18em"}}>Node.js</p>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
           <SiExpress />
           <p style={{fontSize: ".18em"}}>Express.js</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiAmazonaws />
+          <p style={{fontSize: ".18em"}}>AWS</p>
         </Col>
       </Row>
 
@@ -52,6 +73,22 @@ function Techstack() {
       <Row style={{justifyContent: "center"}}>
         <h3>Frontend Technologies</h3>
         <Col xs={4} md={2} className="tech-icons">
+          <DiReact />
+          <p style={{fontSize: ".18em"}}>React.js</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiNextdotjs />
+          <p style={{fontSize: ".18em"}}>Next.js</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiTypescript />
+          <p style={{fontSize: ".18em"}}>TypeScript</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiJavascript1 />
+          <p style={{fontSize: ".18em"}}>JavaScript</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
           <DiHtml5 />
           <p style={{fontSize: ".18em"}}>HTML5</p>
         </Col>
@@ -60,20 +97,12 @@ function Techstack() {
           <p style={{fontSize: ".18em"}}>CSS3</p>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <DiJavascript1 />
-          <p style={{fontSize: ".18em"}}>JavaScript</p>
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiReact />
-          <p style={{fontSize: ".18em"}}>React.js</p>
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiTypescript />
-          <p style={{fontSize: ".18em"}}>TypeScript</p>
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
           <BsBootstrapFill />
           <p style={{fontSize: ".18em"}}>Bootstrap</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiTailwindcss />
+          <p style={{fontSize: ".18em"}}>Tailwincss</p>
         </Col>
       </Row>
 
@@ -90,6 +119,10 @@ function Techstack() {
           <DiPostgresql />
           <p style={{fontSize: ".18em"}}>PostgreSQL</p>
         </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiGraphql />
+          <p style={{fontSize: ".18em"}}>GraphQL</p>
+        </Col>
       </Row>
 
       <br></br>
@@ -98,16 +131,24 @@ function Techstack() {
       <Row style={{justifyContent: "center"}}>
         <h3>Tools and Others</h3>
         <Col xs={4} md={2} className="tech-icons">
+          <SiDocker />
+          <p style={{fontSize: ".18em"}}>Docker</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiJenkins />
+          <p style={{fontSize: ".18em"}}>Jenkins</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiSolidity />
+          <p style={{fontSize: ".18em"}}>Solidity</p>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
           <DiGit />
           <p style={{fontSize: ".18em"}}>Git</p>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
           <DiScrum />
           <p style={{fontSize: ".18em"}}>Scrum | Agile</p>
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiAutodesk />
-          <p style={{fontSize: ".18em"}}>AutoDesk</p>
         </Col>
       </Row>
     </div>
